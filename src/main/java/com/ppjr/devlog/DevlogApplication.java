67c6a705d175b6ab0a1a364d3391e8647ecfa349
Pawel Pykalo <ppykalo@gmail.com>
2018-09-20 14:45:42 +0200

    Add empty Spring Boot project

---
+++ src/main/java/com/ppjr/devlog/DevlogApplication.java
@@ -0,0 +1,12 @@
+package com.ppjr.devlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlogApplication.class, args);
+	}
+}
