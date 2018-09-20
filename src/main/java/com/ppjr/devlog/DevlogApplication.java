6c3dbedbf5467e6327afbc2632a6baeb77e287ba
Pawel Pykalo <ppykalo@gmail.com>
2018-09-20 14:53:25 +0200

    Add hello world / request mapping

--- src/main/java/com/ppjr/devlog/DevlogApplication.java
+++ src/main/java/com/ppjr/devlog/DevlogApplication.java
@@ -2,11 +2,19 @@ package com.ppjr.devlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 @SpringBootApplication
 public class DevlogApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevlogApplication.class, args);
 	}
+
 }
