4dfcd47934c5264354d5cf4c34a557203e5449ce
Pawel Pykalo <ppykalo@gmail.com>
2018-10-09 12:37:39 +0200

    Add Postgres service + switch to Java 8 JDK

--- src/main/java/com/ppjr/devlog/DevlogApplication.java
+++ src/main/java/com/ppjr/devlog/DevlogApplication.java
@@ -1,10 +1,11 @@
 package com.ppjr.devlog;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@EnableAutoConfiguration
 @SpringBootApplication
 public class DevlogApplication {
 
